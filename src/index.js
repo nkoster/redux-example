1e37ef205b28837fd28d9f47ae54edb4117b83df
Niels Koster <n.koster@portavita.eu>
2021-08-08 23:53:42 +0200

    First simple example with a counter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import React from 'react'
 import { render } from 'react-dom'
 import './index.css'
+import { createStore } from 'redux'
 
 import App from './App'
+
+// STORE - Globelized State
+
+// ACTION
+
+const increment = _ => {
+  return {
+    type: 'increment'
+  }
+}
+
+const decrement = _ => {
+  return {
+    type: 'decrement'
+  }
+}
+
+// REDUCER
+
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case 'increment': return state + 1
+    case 'decrement': return state - 1
+  }
+}
+
+let store = createStore(counter)
+
+store.subscribe(_ => console.log(store.getState()))
+
+// DISPATCH
+
+store.dispatch(increment())
+store.dispatch(decrement())
+store.dispatch(decrement())
 
 render(<App />, document.getElementById('root'))
